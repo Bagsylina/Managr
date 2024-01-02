3d901ac5103b21f79d4d3b0ca52de991c5fda9d4
Ilie Dumitru <iliemity@yahoo.com>
2024-01-02 17:37:54 +0200

    Modified home page

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Projects}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
